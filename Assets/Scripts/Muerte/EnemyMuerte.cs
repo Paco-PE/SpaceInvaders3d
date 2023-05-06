99868333ddbbda099ca320b8d934912e592a853e
Paco-PE <pacoprietoescobar@gmail.com>
2023-05-06 21:27:04 +0200

    Añadida la interfaz con puntuación funcionando

--- Assets/Scripts/Muerte/EnemyMuerte.cs
+++ Assets/Scripts/Muerte/EnemyMuerte.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class EnemyMuerte : MonoBehaviour
@@ -8,15 +8,23 @@ public class EnemyMuerte : MonoBehaviour
     
     private AudioManager audioManager;
 
+    private int puntuacion;
+
     void Start(){
         audioManager = FindObjectOfType<AudioManager>();
     }
     void OnDestroy() {
-            // Comprobamos que la escena esté cargada para no instanciar particulas cuando se destruyen los objetos que quedaban en la escena al cerrarla
-            if(this.gameObject.scene.isLoaded){
-                Vector3 posactual = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-                Instantiate(particulasMuerte, posactual, particulasMuerte.transform.rotation);
-                audioManager.ReproducirSonido("invaderkilled");
+        // Comprobamos que la escena esté cargada para no instanciar particulas cuando se destruyen los objetos que quedaban en la escena al cerrarla
+        if(this.gameObject.scene.isLoaded){
+            GameObject puntuacionTextObj = GameObject.Find("ScorePuntuacion");
+            Text puntuacionText = puntuacionTextObj.GetComponent<Text>();
+            puntuacion = int.Parse(puntuacionText.text) + 10;
+            if(puntuacionTextObj != null){
+                puntuacionText.text = puntuacion.ToString();
             }
+            Vector3 posactual = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            Instantiate(particulasMuerte, posactual, particulasMuerte.transform.rotation);
+            audioManager.ReproducirSonido("invaderkilled");
+        }
     }
 }
