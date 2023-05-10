f503b110ad9f2e60a5b2cbcf83695ef19a1b6558
Alejandro <alexcuevascasado@gmail.com>
2023-05-10 19:29:59 +0200

    Carga y cambio de escena

--- Assets/Scripts/Muerte/EnemyMuerte.cs
+++ Assets/Scripts/Muerte/EnemyMuerte.cs
@@ -1,30 +1,38 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyMuerte : MonoBehaviour
 {
     public GameObject particulasMuerte;
-    
     private AudioManager audioManager;
-
     private int puntuacion;
 
-    void Start(){
+    void Start()
+    {
         audioManager = FindObjectOfType<AudioManager>();
     }
-    void OnDestroy() {
-        // Comprobamos que la escena esté cargada para no instanciar particulas cuando se destruyen los objetos que quedaban en la escena al cerrarla
-        if(this.gameObject.scene.isLoaded){
+
+    void OnDestroy()
+    {
+        if (this.gameObject.scene.isLoaded)
+        {
             GameObject puntuacionTextObj = GameObject.Find("ScorePuntuacion");
             Text puntuacionText = puntuacionTextObj.GetComponent<Text>();
             puntuacion = int.Parse(puntuacionText.text) + 10;
-            if(puntuacionTextObj != null){
+            if (puntuacionTextObj != null)
+            {
                 puntuacionText.text = puntuacion.ToString();
             }
             Vector3 posactual = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             Instantiate(particulasMuerte, posactual, particulasMuerte.transform.rotation);
             audioManager.ReproducirSonido("invaderkilled");
+
+            if (puntuacion >= 180)
+            {
+                SceneManager.LoadScene("Nivel2");
+            }
         }
     }
 }
