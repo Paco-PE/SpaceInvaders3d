ae42c6b7b744bf56e28c0fccf46b5f9364a6a415
Paco-PE <pacoprietoescobar@gmail.com>
2023-05-17 17:28:10 +0200

    Musica sonando atraves de todas las pantallas

---
+++ Assets/Scripts/Sounds/MusicaFondo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicaFondo : MonoBehaviour
+{
+    public static MusicaFondo _instancia;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(MusicaFondo._instancia == null){
+            MusicaFondo._instancia = this;
+            DontDestroyOnLoad(gameObject);
+        }else{
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
