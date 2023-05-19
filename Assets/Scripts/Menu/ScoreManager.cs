5dc83218fe3b7433ac310791e53db78256adccb4
Paco-PE <pacoprietoescobar@gmail.com>
2023-05-19 17:04:05 +0200

    Añadido sistema de puntuación y highscore en modo superviviencia

--- Assets/Scripts/Menu/ScoreManager.cs
+++ Assets/Scripts/Menu/ScoreManager.cs
@@ -1,18 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int currentScore = 0;
+    public int highScore = 0;
+    public Text scoreText;
+    public Text highScoreText;
+
+    private void Start()
     {
-        
+        LoadHighScore();
+        UpdateHighScoreText();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddScore(int scoreToAdd)
     {
-        
+        currentScore += scoreToAdd;
+
+        if (currentScore > highScore)
+        {
+            highScore = currentScore;
+            SaveHighScore();
+            UpdateHighScoreText();
+        }
+
+        UpdateScoreText();
+    }
+
+    public void ResetScore()
+    {
+        currentScore = 0;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = currentScore.ToString();
+        }
+    }
+
+    private void UpdateHighScoreText()
+    {
+        if (highScoreText != null)
+        {
+            highScoreText.text = highScore.ToString();
+        }
+    }
+
+    private void SaveHighScore()
+    {
+        PlayerPrefs.SetInt("HighScore", highScore);
+    }
+
+    private void LoadHighScore()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
     }
 }
--- Assets/Scripts/Muerte/EnemyMuerte.cs
+++ Assets/Scripts/Muerte/EnemyMuerte.cs
@@ -1,30 +1,26 @@
 using System.Collections;
-using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EnemyMuerte : MonoBehaviour
 {
     public GameObject particulasMuerte;
+    private ScoreManager scoreManager;
+
     private AudioManager audioManager;
-    private int puntuacion;
+    public int puntuacion;
 
     void Start()
     {
         audioManager = FindObjectOfType<AudioManager>();
+        scoreManager = FindObjectOfType<ScoreManager>();
     }
 
     void OnDestroy()
     {
         if (this.gameObject.scene.isLoaded)
         {
-            GameObject puntuacionTextObj = GameObject.Find("ScorePuntuacion");
-            Text puntuacionText = puntuacionTextObj.GetComponent<Text>();
-            puntuacion = int.Parse(puntuacionText.text) + 10;
-            if (puntuacionTextObj != null)
-            {
-                puntuacionText.text = puntuacion.ToString();
-            }
+            scoreManager.AddScore(10);
             Vector3 posactual = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             Instantiate(particulasMuerte, posactual, particulasMuerte.transform.rotation);
             audioManager.ReproducirSonido("invaderkilled");
