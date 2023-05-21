c906ca015a520a5131bd517820da0723405eda33
Paco-PE <pacoprietoescobar@gmail.com>
2023-05-21 10:04:33 +0200

    Arreglado problema cuando se mataba al jefe el último en el nivel 3

--- Assets/Scripts/Muerte/EnemyMuerte.cs
+++ Assets/Scripts/Muerte/EnemyMuerte.cs
@@ -26,6 +26,21 @@ public class EnemyMuerte : MonoBehaviour
             audioManager.ReproducirSonido("invaderkilled");
 
             GameObject[] enemigos = GameObject.FindGameObjectsWithTag("Enemy");
+            GameObject boss = GameObject.FindGameObjectWithTag("Boss");
+            if(boss != null){
+                // Crear un nuevo vector con espacio para el objeto "Boss" y los enemigos existentes
+                GameObject[] enemigosConBoss = new GameObject[enemigos.Length + 1];
+                
+                // Copiar los objetos "Enemy" al nuevo vector
+                enemigos.CopyTo(enemigosConBoss, 0);
+                
+                // Añadir el objeto "Boss" al final del vector
+                enemigosConBoss[enemigos.Length] = boss;
+                
+                // Reemplazar el vector de enemigos original por el nuevo vector que incluye al "Boss"
+                enemigos = enemigosConBoss;
+            }
+            
             if (enemigos.Length == 0)
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
